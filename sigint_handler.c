536e7511978853726d04f9fea480d6e0fa297e86
Bits2green <neshmunesh2@gmail.com>
2023-10-12 10:54:07 +0300

    fix

--- sigint_handler.c
+++ sigint_handler.c
@@ -1,6 +1,7 @@
 #include "shell.h"
 
 void sigint_handler(int signum) {
+    (void)signum;
     if (current_child_pid > 0) {
         /*Terminate the currently running command*/
         kill(current_child_pid, SIGKILL);
--- sigtstp_handler.c
+++ sigtstp_handler.c
@@ -1,6 +1,7 @@
 #include "shell.h"
 
 void sigtstp_handler(int signum) {
+    (void)signum;
     if (current_child_pid > 0) {
         /*Suspend the currently running command*/
         kill(current_child_pid, SIGSTOP);
