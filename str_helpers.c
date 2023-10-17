8280941dffeef44a27a8240d1284a4b4ef6a4380
Bits2green <neshmunesh2@gmail.com>
2023-10-17 09:30:31 +0300

    Fix

--- str_helpers.c
+++ str_helpers.c
@@ -1,6 +1,12 @@
 #include "shell.h"
 
-/*Custom str functions*/
+/**
+ * my_strcmp - Compares strings
+ * @s1: String 1
+ * @s2: String 2
+ * 
+ * Return: 0
+*/
 int my_strcmp(const char *s1, const char *s2) {
     while (*s1 && (*s1 == *s2)) {
         s1++;
@@ -8,6 +14,13 @@ int my_strcmp(const char *s1, const char *s2) {
     }
     return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
+
+/**
+ * my_strlen - Calculates length of string
+ * @str: String whose length to be calcutated
+ * 
+ * Return: 0
+*/
 
 size_t my_strlen(const char *str) {
     const char *s = str;
@@ -17,14 +30,28 @@ size_t my_strlen(const char *str) {
     return (s - str);
 }
 
-char *my_strcpy(char *dest, const char *src) {
+/**
+ * my_strcpy - Copies strings
+ * @dest: Destination of the copied string
+ * @src: String to be copied
+ * 
+ * Return: 0
+*/
+
+char *my_strcpy(char *dest, const char *src)
     char *d = dest;
     while ((*d = *src)) {
         d++;
         src++;
     }
     return (dest);
-}
+
+/**
+ * my_strdup - duplicates a string
+ * @str: String to be duplicated
+ * 
+ * Return: 0
+*/
 
 char *my_strdup(const char *str)
 {
