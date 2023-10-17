64e6c4b545c94d24187056d52c4bd9f129aacd7b
Bits2green <neshmunesh2@gmail.com>
2023-10-18 00:00:45 +0300

    fr

--- shell.h
+++ shell.h
@@ -26,7 +26,7 @@ int startsWithSlash(char *input);
 int isExitCommand(char *userInput, char *exitCommand);
 int isEnvCommand(char *userInput, char *envCommand);
 char **parseInput(char *input);
-void handleCtrlC(int signal);
+void controlC(int signal);
 
 /*shell_prompt.c*/
 void runShell(void);
