3f081cd60d3687c4ca4575e83adb68166b0855ac
Bits2green <neshmunesh2@gmail.com>
2023-10-17 10:49:46 +0300

    Push

--- shell.c
+++ shell.c
@@ -1,7 +1,7 @@
 #include "shell.h"
-<<<<<<< HEAD
 
-int main(int argc, char *argv[], char *envp[]) {
+int main(int argc, char *argv[], char *envp[])
+{
     char *input = NULL;
     size_t input_size = 0;
     ssize_t read_size;
@@ -12,76 +12,40 @@ int main(int argc, char *argv[], char *envp[]) {
     (void)argc, (void)argv;
     
     while (1) {
-        if (interactive) {
+        if (interactive)
+        {
             my_printf("smshell$ ");
-            fflush(stdout);
-        }
-        
+            fflush(stdout); }
         read_size = getline(&input, &input_size, stdin);
-        
-        if (read_size == -1) {
-            if (interactive) {
+        if (read_size == -1)
+        {
+            if (interactive)
                 my_printf("\n");
-            }
             free(input);
-            break;
-        }
-        
+            break; }
         parse_input(input, &command, envp);
-        
-        if (command.name != NULL) {
-            if (my_strcmp(command.name, "cd") == 0) {
+        if (command.name != NULL)
+        {
+            if (my_strcmp(command.name, "cd") == 0)
+            {
                 if (chdir(command.args[0]) == -1)
-                    my_printf("./vcsh: Failed to change directory\n");
-            } else if (my_strcmp(command.name, "exit") == 0) {
+                    my_printf("./vcsh: Failed to change directory\n"); }
+            else if (my_strcmp(command.name, "exit") == 0)
                 exit_shell();
-            } else if (my_strcmp(command.name, "env") == 0) {
+            else if (my_strcmp(command.name, "env") == 0)
                 print_environment(envp);
-            } else {
+            else 
+            {
                 if (execute_command_with_path(&command, envp) == -1)
-                    my_printf("./vcsh: Command not found.\n");
-            }
-        }
-        
-        if (input != NULL) {
+                    my_printf("./vcsh: Command not found.\n"); } }
+        if (input != NULL)
+        {
             free(input);
-            input = NULL;
-        }
-        
-        if (command.name != NULL) {
+            input = NULL; }
+        if (command.name != NULL)
+        {
             free(command.name);
             for (i = 0; command.args[i] != NULL; i++)
                 free(command.args[i]);
-            memset(&command, 0, sizeof(struct Command));
-        }
-    }
-    return (0);
-=======
-/**
-* main - entry point
-* @argc: args
-* @argv: args list
-*
-* Return: 0
-*/
-int main(int argc, char *argv[])
-{
-int interactive = isatty(STDIN_FILENO);
-char input[MAX_INPUT_SIZE];
-(void)argc; (void)argv;
-while (1)
-{
-if (interactive)
-{
-printf("vcshell$ ");
-fflush(stdout); }
-if (fgets(input, sizeof(input), stdin) == NULL)
-{
-if (interactive)
-printf("\n");
-break; }
-}
-
-return (0);
->>>>>>> 539537b932059b760d5831f91ae54583e50f4972
-}
+            memset(&command, 0, sizeof(struct Command)); } }
+    return (0); }
