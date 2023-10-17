3267d26f9845496ea9e4b6cb63a88901de5f87c4
Bits2green <neshmunesh2@gmail.com>
2023-10-17 12:16:54 +0300

    fix

--- more_print_helpers.c
+++ more_print_helpers.c
@@ -111,4 +111,38 @@ n = -num;
 sign = '-';
 }
 
-array = flags & CONVERT_LOWERCASE ? "0123456789
+array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+    ptr = &buffer[49];
+    *ptr = '\0';
+
+    do
+    {
+        *--ptr = array[n % base];
+        n /= base;
+    } while (n != 0);
+
+    if (sign)
+        *--ptr = sign;
+    
+    return ptr;
+}
+
+/**
+ * custom_remove_comments - Replaces the first instance of '#' with '\0'.
+ * @buf: Address of the string to modify.
+ *
+ * Return: Always 0.
+ */
+void custom_remove_comments(char *buf)
+{
+    int i;
+
+    for (i = 0; buf[i] != '\0'; i++)
+    {
+        if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+        {
+            buf[i] = '\0';
+            break;
+        }
+    }
+}
--- shell.h
+++ shell.h
@@ -12,6 +12,35 @@
 #include <fcntl.h>
 #include <errno.h>
 
+/*print_helpers.c*/
+void custom_eputs(char *str);
+int custom_eputchar(char c);
+int custom_putfd(char c, int fd);
+int custom_putsfd(char *str, int fd);
+
+/*more_print_helpers.c*/
+int custom_erratoi(char *s);
+void custom_print_error(info_t *info, char *estr);
+int custom_print_d(int input, int fd);
+char *custom_convert_number(long int num, int base, int flags);
+void custom_remove_comments(char *buf);
+
+/*string_helpers.c*/
+int custom_strlen(char *str);
+int custom_strcmp(char *str1, char *str2);
+char *custom_starts_with(const char *str, const char *prefix);
+char *custom_strcat(char *destination, char *source);
+
+/*more_string_helpers.c*/
+char *custom_strcpy(char *dest, char *src);
+char *custom_strdup(const char *str);
+void custom_puts(char *str);
+int custom_putchar(char c);
+
+/*reallocate_helper.c*/
+char *custom_memset(char *s, char b, unsigned int n);
+void custom_ffree(char **pp);
+void *custom_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 
 
