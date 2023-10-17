941c63d6f4a328783e317b5d4a472e1fe5418aee
Bits2green <neshmunesh2@gmail.com>
2023-10-17 08:48:42 +0000

    Print Helpers

--- print_helpers.c
+++ print_helpers.c
@@ -1,92 +1,92 @@
 #include "shell.h"
 
 /**
- * custom_eputs - Prints an input string to stderr.
- * @str: The string to be printed.
- *
- * Return: Nothing.
- */
+* custom_eputs - Prints an input string to stderr.
+* @str: The string to be printed.
+*
+* Return: Nothing.
+*/
 void custom_eputs(char *str)
 {
-    int i = 0;
+int i = 0;
 
-    if (!str)
-        return;
+if (!str)
+return;
 
-    while (str[i] != '\0')
-    {
-        custom_eputchar(str[i]);
-        i++;
-    }
+while (str[i] != '\0')
+{
+custom_eputchar(str[i]);
+i++;
+}
 }
 
 /**
- * custom_eputchar - Writes the character c to stderr.
- * @c: The character to print.
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* custom_eputchar - Writes the character c to stderr.
+* @c: The character to print.
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 int custom_eputchar(char c)
 {
-    static int i;
-    static char buf[WRITE_BUF_SIZE];
+static int i;
+static char buf[WRITE_BUF_SIZE];
 
-    if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-    {
-        write(2, buf, i);
-        i = 0;
-    }
+if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+{
+write(2, buf, i);
+i = 0;
+}
 
-    if (c != BUF_FLUSH)
-        buf[i++] = c;
+if (c != BUF_FLUSH)
+buf[i++] = c;
 
-    return (1);
+return (1);
 }
 
 /**
- * custom_putfd - Writes the character c to the given file descriptor.
- * @c: The character to print.
- * @fd: The file descriptor to write to.
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* custom_putfd - Writes the character c to the given file descriptor.
+* @c: The character to print.
+* @fd: The file descriptor to write to.
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 int custom_putfd(char c, int fd)
 {
-    static int i;
-    static char buf[WRITE_BUF_SIZE];
+static int i;
+static char buf[WRITE_BUF_SIZE];
 
-    if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-    {
-        write(fd, buf, i);
-        i = 0;
-    }
+if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+{
+write(fd, buf, i);
+i = 0;
+}
 
-    if (c != BUF_FLUSH)
-        buf[i++] = c;
+if (c != BUF_FLUSH)
+buf[i++] = c;
 
-    return (1);
+return (1);
 }
 
 /**
- * custom_putsfd - Prints an input string to the specified file descriptor.
- * @str: The string to be printed.
- * @fd: The file descriptor to write to.
- *
- * Return: The number of characters written.
- */
+* custom_putsfd - Prints an input string to the specified file descriptor.
+* @str: The string to be printed.
+* @fd: The file descriptor to write to.
+*
+* Return: The number of characters written.
+*/
 int custom_putsfd(char *str, int fd)
 {
-    int i = 0;
+int i = 0;
 
-    if (!str)
-        return (0);
+if (!str)
+return (0);
 
-    while (*str)
-    {
-        i += custom_putfd(*str++, fd);
-    }
+while (*str)
+{
+i += custom_putfd(*str++, fd);
+}
 
-    return (i);
+return (i);
 }
