9e06015fde424da3b3ca38e7eb351b3f5b266b72
Bits2green <neshmunesh2@gmail.com>
2023-10-17 07:37:23 +0000

    Shell

--- shell.c
+++ shell.c
@@ -8,21 +8,21 @@
 */
 int main(int argc, char *argv[])
 {
-    int interactive = isatty(STDIN_FILENO);
-    char input[MAX_INPUT_SIZE];
+int interactive = isatty(STDIN_FILENO);
+char input[MAX_INPUT_SIZE];
 
-    while (1)
-    {
-        if (interactive)
-        {
-            printf("vcshell$ ");
-            fflush(stdout); }
-        if (fgets(input, sizeof(input), stdin) == NULL)
-        {
-            if (interactive)
-                printf("\n");
-            break; }
-    }
+while (1)
+{
+if (interactive)
+{
+printf("vcshell$ ");
+fflush(stdout); }
+if (fgets(input, sizeof(input), stdin) == NULL)
+{
+if (interactive)
+printf("\n");
+break; }
+}
 
-    return (0);
+return (0);
 }
