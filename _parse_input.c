f8ea796c2b2ceeb59f27ea161451e1010cf028f7
Bits2green <neshmunesh2@gmail.com>
2023-10-18 09:26:10 +0300

    fr

---
+++ _parse_input.c
@@ -0,0 +1,63 @@
+#include "shell.h"
+
+/**
+* parse_input - parses the command provided by the user
+* @input: the command
+* @Command: Structure to handle the input
+* @envp: provides the PATH to the executable in the /bin folder
+*
+* Return: 0
+*/
+
+void parse_input(char *input, struct Command *command, char **envp) {
+
+size_t i;
+char *token;
+int arg_index = 0;
+int is_command_name_set;
+
+/*Initialize the command's arguments*/
+for (i = 0; i < sizeof(command->args) / sizeof(command->args[0]); i++) {
+command->args[i] = NULL;
+}
+
+/*Tokenize the input to separate the command name and its arguments*/
+token = strtok(input, " \t\n"); /*Tokenize based on space, tab, and newline*/
+
+/*Initialize the argument index*/
+arg_index = 0;
+
+/*Initialize a flag to determine if we've encountered the command name*/
+is_command_name_set = 0;
+
+while (token != NULL) {
+if (!is_command_name_set) {
+/*The first token is the command name*/
+command->name = my_strdup(token);
+is_command_name_set = 1;
+} else {
+/*Subsequent tokens are arguments*/
+/*Expand environment variables in arguments*/
+if (token[0] == '$') {
+char *var_name = token + 1; /*Skip the '$'*/
+char *var_value = custom_getenv(var_name, envp);
+if (var_value != NULL) {
+/*Replace the argument with the environment variable value*/
+command->args[arg_index] = my_strdup(var_value);
+} else {
+/*Environment variable not found, keep the original argument*/
+command->args[arg_index] = my_strdup(token);
+}
+} else {
+/*Not an environment variable, keep the original argument*/
+command->args[arg_index] = my_strdup(token);
+}
+
+/*Move to the next argument*/
+arg_index++;
+}
+
+/*Get the next token*/
+token = strtok(NULL, " \t\n");
+}
+}
--- shell.h
+++ shell.h
@@ -1,37 +1,63 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <sys/wait.h>
-#include <sys/types.h>
+#define MAX_INPUT_SIZE 1024
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include <unistd.h>
-#include <dirent.h>
-#include <limits.h>
-#include <string.h>
-#include <signal.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+/*#include <signal.h>*/
 
-int compare_strings(char *str1, char *str2);
-int write_char(char ch);
-void print_string(char *str);
-int string_length(char *str);
-char *concatenate_strings(char *s1, char *s2);
+/* Defining Structures */
 
-/*helper_funcs1.c*/
-int startsWithSlash(char *input);
-int isExitCommand(char *userInput, char *exitCommand);
-int isEnvCommand(char *userInput, char *envCommand);
-char **parseInput(char *input);
-void controlD(int signal);
+struct Command {
+    char *name;
+    char *args[32]; /*Adjust the size as needed*/
+};
 
-/*shell_prompt.c*/
-void runShell(void);
+/* ++++++++++++++++ Functions ++++++++++++++++++++++*/
 
-void executeCommand(char **command);
+/*mv_print.c*/
+void my_printf(const char *format, ...);
 
-char *findExecutablePath(char *command);
+/* _parse_input.c */
+void parse_input(char *input, struct Command *command, char **envp);
+
+/*excecute_cmd_v1.c*/
+int execute_command(struct Command *command);
+
+/*sigint_handler.c*/
+void sigint_handler(int signum);
+
+/*expandvars.c*/
+void expand_env_vars(struct Command *command);
+
+/*_getenv.c*/
+char *custom_getenv(const char *var_name, char **envp);
+
+/*execute_cmd_v2.c*/
+int execute_command_with_path(struct Command *command, char **envp);
+
+/*builtins.c*/
+void exit_shell();
+void print_environment(char **envp);
+
+/*str_helpers.c*/
+char *my_strdup(const char *str);
+int my_strcmp(const char *s1, const char *s2);
+size_t my_strlen(const char *str);
+char *my_strcpy(char *dest, const char *src);
+
+/*print_helpers.c*/
+int my_snprintf(char *str, size_t size, const char *format, ...);
+void my_putint(int num);
+void my_putchar(char c);
+void my_puts(const char *str);
 
 
 #endif
